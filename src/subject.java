f781b2c732fecdadec50a0409fb8707851eef2a3
Mariamhilall <93837690+Mariamhilall@users.noreply.github.com>
2021-11-23 22:13:16 +0300

    subject class added

---
+++ src/subject.java
@@ -0,0 +1,7 @@
+public interface subject {
+    public interface subject {
+        void registerObserver (observer observer);
+        void removeObserver (observer observer);
+        void notifyObserver ();
+    }
+}
