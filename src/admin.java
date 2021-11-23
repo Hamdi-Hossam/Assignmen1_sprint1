fa8bfe722f1f268c72dbeea4c09c36bd850c83ea
Hamdi <hamdihossam461@gmail.com>
2021-11-23 20:55:53 +0200

    admin is added

---
+++ src/admin.java
@@ -0,0 +1,6 @@
+public class admin {
+    public void verifyRegistration(userManager um){
+        um.DriverRegister.put("verify", "True");
+        System.out.println("THE ADMIN ACCEPT YOUR REQUEST YOU CAN LOGIN NOW");
+    }
+}
